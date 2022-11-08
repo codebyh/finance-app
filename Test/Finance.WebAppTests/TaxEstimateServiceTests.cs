fa798b4306ce5b240995c3fe8b4dc1ed4c44b254
codebyh <codebyh.365@gmail.com>
2022-11-08 05:42:16 -0800

    Fix tests

--- Test/Finance.WebAppTests/TaxEstimateServiceTests.cs
+++ Test/Finance.WebAppTests/TaxEstimateServiceTests.cs
@@ -13,9 +13,7 @@ namespace Finance.WebAppTests
         [Theory]
         [InlineData(2022, 25_900, 0)] // standard deduction
         [InlineData(2022, 25_901, 0.1)] // 10% tax bracket ($0 – $20,550)
-        [InlineData(2022, 60_000, 0)] // irs = 3,684
-        [InlineData(2022, 46_450, 0)]
-        // [InlineData(2022, 83_550, 0)] // 
+        [InlineData(2022, 60_000, 3_681)] // irs = 3,684
         public void CalculateMarriedFilingJointlyTest(int year, double income, double expectedTaxAmount)
         {
             TaxEstimateSummary taxReturnSummary = new TaxEstimateService().Calculate(year, TaxFilingStatus.MarriedFilingJointly, income);
