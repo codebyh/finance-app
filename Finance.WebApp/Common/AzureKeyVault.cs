0d0bde7ad63dc11f797a1a3dc87b7a9012ea8fa5
codebyh <codebyh.365@gmail.com>
2022-11-10 09:30:16 -0800

    Add project for common services

---
+++ Finance.WebApp/Common/AzureKeyVault.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Finance.WebApp.Common
+{
+    public class AzureKeyVault
+    {
+        SecretClient client;
+
+        public AzureKeyVault()
+        {
+
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                     }
+            };
+            client = new SecretClient(new Uri("https://<your-unique-key-vault-name>.vault.azure.net/"), new DefaultAzureCredential(), options);
+        }
+
+        string GetSecret(string secretName)
+        {
+            KeyVaultSecret secret = client.GetSecret("<mySecret>");
+
+            return secret?.Value;
+        }
+
+    }
+}
